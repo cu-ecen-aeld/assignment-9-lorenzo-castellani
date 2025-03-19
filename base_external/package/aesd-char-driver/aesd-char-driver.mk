433b720d88b5569ba70fc10e335514ce52f70517
lorenzo-castellani <lorenzo.castellani@pd.infn.it>
2025-03-19 10:27:16 +0100

    assignment-9 done
    Signed-off-by: lorenzo-castellani <lorenzo.castellani@pd.infn.it>

--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-AESD_CHAR_DRIVER_VERSION = 7d98bb7cae8374861569f8f0d92b11d946fd7ab9
+AESD_CHAR_DRIVER_VERSION = b9822df117818437d72f33f8695da2c3ed5250d1
 AESD_CHAR_DRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-lorenzo-castellani.git
 AESD_CHAR_DRIVER_SITE_METHOD = git
 AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
